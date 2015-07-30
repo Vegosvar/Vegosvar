8f66c3b882c9d6d6bdf7a36755ece7e502ca9f65
DMarby <david@dmarby.se>
2015-07-30 04:02:58 +0200

    Default to prod env for now

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'development') {
+  process.env.NODE_ENV = 'production'
+}
+
 var passport = require('passport')
 var facebook_strategy = require('passport-facebook').Strategy
 
