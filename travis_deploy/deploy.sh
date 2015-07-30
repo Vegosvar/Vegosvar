bbc1fa28ce1278a7ae2f400bcac8a56fab3de0a7
DMarby <david@dmarby.se>
2015-07-30 03:10:36 +0200

    Disable host key checking

--- travis_deploy/deploy.sh
+++ travis_deploy/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ev
 
-ssh vegosvar@dev.vegosvar.se -i deploy_key 'cd /opt/vegosvar && git pull && npm install && gulp prod && sudo service vegosvar restart'
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no vegosvar@dev.vegosvar.se -i deploy_key 'cd /opt/vegosvar && git pull && npm install && gulp prod && sudo service vegosvar restart'
