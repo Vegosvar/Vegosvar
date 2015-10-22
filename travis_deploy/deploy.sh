850496593a08b154dea612eecdecf0a64410da11
Per Lööv <per.loov@gmail.com>
2015-10-23 00:13:12 +0200

    Search improvements + Removed Travis

--- travis_deploy/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -ev
-chmod 400 travis_deploy/deploy_key
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no vegosvar@dev.vegosvar.se -i travis_deploy/deploy_key 'cd /opt/vegosvar && git pull && npm install && gulp prod && sudo service vegosvar restart'
