3d1bb63397f9d863dfc7a2a86f93f47a7f689462
didair <didair@msn.com>
2015-11-15 00:50:03 +0100

    Further work on image processing

--- imageProcesser.js
+++ imageProcesser.js
@@ -3,8 +3,12 @@ var fs = require('fs')
 
 exports.resize = function(filename, width, height) {
 	var image = sharp('uploads/' + filename + '_original.jpg')
-	image.resize(width, height)
-
-	image.toFile('uploads/' + filename + '.jpg', function (err, info) { })
+	var image_small = image
+	image.resize(1920, undefined)
+	image.quality(90)
+	image.toFile('/uploads/' + filename + '._original.jpg', function (err, info) { })
+	image_small.resize(width, height)
+	image_small.quality(90)
+	image_small.toFile('uploads/' + filename + '.jpg', function (err, info) { })
 	return true
 }
