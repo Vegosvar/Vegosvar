f9c90b154aa7824d3e8d38ba41976cb651da96e7
didair <didair@msn.com>
2015-11-15 01:18:50 +0100

    More work on ze image processer

--- imageProcesser.js
+++ imageProcesser.js
@@ -3,13 +3,19 @@ var fs = require('fs')
 
 exports.resize = function(filename, width, height) {
 	var image = sharp('uploads/' + filename + '_original.jpg')
-	var imagesmall = image
-	image.resize(1920, undefined)
+	image.resize(1920, 1080)
 	image.quality(90)
-	image.toFile('/uploads/' + filename + '._default.jpg', function (err, info) { })
+	image.toFile('uploads/' + filename + '_processed.jpg', function (err, info) { })
 	
+	var imagesmall = sharp('uploads/' + filename + '_original.jpg')
 	imagesmall.resize(width, height)
-	imagesmall.quality(90)
 	imagesmall.toFile('uploads/' + filename + '.jpg', function (err, info) { })
+
+	var imagethumb = sharp('uploads/' + filename + '_original.jpg')
+	imagethumb.resize(345, 181)
+	imagethumb.toFile('uploads/' + filename + '_thumb.jpg', function (err, info) { 
+		fs.unlink(__dirname + '/uploads/' + uFilename + '_original.jpg')
+	})
+	
 	return true
 }
