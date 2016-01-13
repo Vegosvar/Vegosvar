5110f98455e8f7303019aea0c31e0522b865f297
Tobias Järvelöv <tobias@jarvelov.se>
2016-01-13 09:46:23 +0000

    Better redirect logic

--- functions.js
+++ functions.js
@@ -29,13 +29,36 @@ module.exports = {
 		var dd  = date.getUTCDate().toString()
 		return yyyy + '-' + (mm[1]?mm:"0"+mm[0]) + '-' + (dd[1]?dd:"0"+dd[0])
 	},
-    getPrettyDateTime: function(date) {
-        year = "" + date.getUTCFullYear();
-        month = "" + (date.getUTCMonth() + 1); if (month.length == 1) { month = "0" + month; }
-        day = "" + date.getUTCDate(); if (day.length == 1) { day = "0" + day; }
-        hour = "" + date.getHours(); if (hour.length == 1) { hour = "0" + hour; }
-        minute = "" + date.getUTCMinutes(); if (minute.length == 1) { minute = "0" + minute; }
-        second = "" + date.getUTCSeconds(); if (second.length == 1) { second = "0" + second; }
-        return year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second;
-	}
+  getPrettyDateTime: function(date) {
+    year = "" + date.getUTCFullYear();
+    month = "" + (date.getUTCMonth() + 1); if (month.length == 1) { month = "0" + month; }
+    day = "" + date.getUTCDate(); if (day.length == 1) { day = "0" + day; }
+    hour = "" + date.getHours(); if (hour.length == 1) { hour = "0" + hour; }
+    minute = "" + date.getUTCMinutes(); if (minute.length == 1) { minute = "0" + minute; }
+    second = "" + date.getUTCSeconds(); if (second.length == 1) { second = "0" + second; }
+    return year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second;
+	},
+  returnUrl: function(req) {
+    previousUrl = req.headers.referer
+    host = req.headers.host
+
+    if(typeof(previousUrl) !== 'undefined') {
+      if(previousUrl.indexOf(host) >= 0) {
+        var page = ( previousUrl.substr(previousUrl.indexOf(host) + host.length) )
+        var url = page
+
+        if ( req.session.returnTo !== 'undefined' ) {
+          if( req.session.returnTo === '/' && page === '/' && req.originalUrl.indexOf('.') === -1 ) {
+            url = req.originalUrl
+          }
+        }
+
+        return url
+      } else {
+        return req.originalUrl
+      }
+    } else {
+      return req.originalUrl
+    }
+  }
 }
