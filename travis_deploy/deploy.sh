985599d4c4321f4dca4fc1570305761f92a0ef7f
Tobias Järvelöv <tobias@jarvelov.se>
2016-02-16 12:07:20 +0000

    Master specific branch changes

--- travis_deploy/deploy.sh
+++ travis_deploy/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ev
 chmod 400 travis_deploy/deploy_key
-/usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no vegosvar@dev.vegosvar.se -i travis_deploy/deploy_key 'cd /var/www/dev.vegosvar.se && git pull origin dev && npm install && node tools/devconfigure.js'
+/usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no vegosvar@beta.vegosvar.se -i travis_deploy/deploy_key 'cd /var/www/beta.vegosvar.se && git pull origin master && npm install'
