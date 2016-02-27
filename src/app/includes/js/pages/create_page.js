9409f7186dfbaccc5432bfb2cf4988a33dd5964d
Tobias Järvelöv <tobias@jarvelov.se>
2016-02-27 17:50:30 +0000

    Rename page to create_page to describe what it does

---
+++ src/app/includes/js/pages/create_page.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  function checkLicenseVal (value) {
+    if (value === 'CC BY') {
+      $('.cc-version').fadeIn()
+    } else {
+      $('.cc-version').hide()
+    }
+  }
+
+  $('#license_select').on('change', function () {
+    checkLicenseVal($(this).val())
+  })
+
+  checkLicenseVal($('#license_select').val())
+})
