ae348793d04c6abca9c27ff79a8382d914ba2899
Tobias Järvelöv <tobias@jarvelov.se>
2016-02-28 01:57:49 +0000

    Complete rebuild of search query. Regex can still be used on the text index, and this aggregation query is pretty fast, not as fast though, but it is more accurate

--- src/app/routes/ajax/unauthenticated.js
+++ src/app/routes/ajax/unauthenticated.js
@@ -7,6 +7,7 @@
 
 var ObjectID = require('mongodb').ObjectID
 var striptags = require('striptags')
+var extend = require('util')._extend;
 
 module.exports = function (app, resources) {
   /** /ajax/map
@@ -80,73 +81,217 @@ module.exports = function (app, resources) {
   */
   app.get('/ajax/search', function (req, res) {
     var searchString = req.query.s
-
-    //The default query
-    var query = {
-      accepted: true, //Only published pages
-      $text: {
-        $search: searchString
-      }
-    }
-
-    //The default options
-    var options = {
-      "score": {
-        $meta: "textScore",
-      }
-    }
-
-    //The default sort options
-    var sort = {
-      "score": {
-        $meta: "textScore"
-      }
-    }
-
-    //Check if query matches a type
     var searchArray = searchString.toLowerCase().split(' ')
 
+    //The default query object
+    var query = {}
+
+    //The default fields we search
+    var searchFields = ['url','title','post.content','post.food','post.product_type','post.city']
+
+    //Check if query matches a type
     var queryType = false
-    for (var i in searchArray) {
-      switch (searchArray[i]) {
-        case 'butik':
-        case 'butiker':
-          queryType = "5"
-          break
-        case 'restaurang':
-        case 'restauranger':
-          queryType = "3"
-          break
-        case 'produkt':
-        case 'produkter':
-          queryType = "4"
-          break
-      }
-    }
 
-    if(queryType) {
-      query['type'] = queryType
-    }
-
-    var citiesdb = resources.collections.cities
-    citiesdb.find({name:{$in:searchArray}}).toArray(function(err, doc) {
-      if(err) throw err
-      if(doc.length > 0) {
-        query['post.city'] = {
-          '$regex': doc[0].name, //Search only this city
-          '$options': '-i'
+    var queryOperations = {
+      'butik': function() {
+        query['type'] = '5'
+        return true
+      },
+      'produkt': function() {
+        query['type'] = '4'
+        return true
+      },
+      'restaurang': function() {
+        query['type'] = '3'
+        return true
+      },
+      'recept': function() {
+        query['type'] = '2'
+        return true
+      },
+      'fakta': function() {
+        query['type'] = '1'
+        return true
+      },
+      'vegan': function() {
+        if('type' in query && query.type === '3') {
+          query['veg_type'] = 'vegan'
+          return true
+        } else {
+          searchFields.push({
+            property: 'post.veg_type',
+            value: 'vegan'
+          })
+          return false
+        }
+      },
+      'laktoovo': function() {
+        if('type' in query && query.type === '3') {
+          query['veg_type'] = 'lacto_ovo'
+          return true
+        } else {
+          searchFields.push({
+            property: 'post.veg_type',
+            value: 'lacto_ovo'
+          })
+          return false
+        }
+      },
+      'animal': function() {
+        if('type' in query && query.type === '3') {
+          query['veg_type'] = 'animal'
+          return true
+        } else {
+          searchFields.push({
+            property: 'post.veg_type',
+            value: 'animal'
+          })
+          return false
         }
       }
+    }
+
+    var queryTypes = function(string) {
+      string = string.toLowerCase().replace(/[^a-z]/gi,'') //Remove non alphabet characters
+
+      var keywords = ['butik','restaurang','produkt','recept','fakta','vegan','laktoovo','animal']
+      var regexMatches = new RegExp('^' + keywords.join('|'))
+
+      var match = string.match(regexMatches)
+      if(match !== null && match.length > 0) {
+        var key = match[0]
+
+        if(key in queryOperations) {
+          //Perform the operations on the search query
+          if(queryOperations[key]()) {
+            //Update the search string to remove the key from the search, otherwise might result in unwanted results
+
+            var tmpArray = searchString.split(' ')
+
+            searchString = tmpArray.filter(function(text) {
+              var isKey = text.match(key)
+              if(isKey === null) {
+                return text
+              }
+            }).join(' ').trim()
+          }
+        }
+      }
+    }
+
+    for (var i in searchArray) {
+      queryTypes(searchArray[i])
+    }
+
+    //Build general query
+    var regexTitleArray = []
+
+    //As the searchString has been manipulated it might not even be anything at all anymore
+    if(searchString.length > 0) {
+      regexTitleArray = searchString.split(' ').map(function(text) {
+        return new RegExp(text, 'i')
+      })
+    }
+
+    console.log(regexTitleArray)
+
+    var orFields = []
+    for (var i = searchFields.length - 1; i >= 0; i--) {
+      var obj = {}
+
+      if(typeof searchFields[i] === 'object') {
+        if('property' in searchFields[i] && 'value' in searchFields[i]) {
+          obj[searchFields[i].property] = {
+            $in: [searchFields[i].value]
+          }
+        }
+      } else {
+        obj[searchFields[i]] = {
+          $in: regexTitleArray
+        }
+      }
+
+      orFields.push(obj)
+    }
+
+    var filteredQuery = {
+      $match: {
+        accepted: true, //Only published pages
+        $or: orFields
+      }
+    }
+
+    for(var key in query) {
+      if( ! (key in filteredQuery.$match) ) {
+        filteredQuery.$match[key] = query[key]
+      }
+    }
+
+    //Check if query matches a city
+    var citiesdb = resources.collections.cities
+    citiesdb.find({
+      name: {
+        $in: searchArray
+      }
+    }).toArray(function(err, cities) {
+      if(err) throw err
+
+      if(cities.length > 0) {
+        filteredQuery.$match['post.city'] = {
+          '$regex': cities[0].name, //Search only this city
+          '$options': '-i'
+        }
+
+      }
+
+      /*
+      //Check if query matches a category
+      var categoriesdb = resources.collections.categories
+      categoriesdb.find({
+        $or: [{
+          name: {
+            $in: searchArray
+          }
+        }, {
+          subcategory: {
+            $in: searchArray
+          }
+        }]
+      }).toArray(function(err, categories) {
+        //console.log(categories)
+        //If we found a match here, we might want to restrict the query to that type of category
+      })
+      */
+
+      //TODO build aggregate query better, utilizing $meta: "textScore"
+      //Maybe rebuild results with projection or just use group
+
+      console.log(filteredQuery.$match)
 
       //Find pages
       var pagesdb = resources.collections.pages
-
-      pagesdb.find(query, options).sort(sort).toArray(function (err, doc) {
+      pagesdb.aggregate([filteredQuery], function (err, pages) {
         if(err) throw err
-        for (var i = 0; i < doc.length; i++) {
-          doc[i].post.content = striptags(doc[i].post.content, ['br','p','a','span','i','b'])
+
+        for (var i = 0; i < pages.length; i++) {
+          if('post' in pages[i]) {
+            if('content' in pages[i].post) {
+              pages[i].post.content = striptags(pages[i].post.content, ['br','p','a','span','i','b'])
+            } else {
+              delete(pages[i])
+            }
+          } else {
+            delete(pages[i])
+          }
         }
-        res.json(doc)
+
+        //Reindex array
+        pages = pages.filter(function(page) {
+          return page
+        })
+
+
+        res.json(pages)
       })
     })
   })
