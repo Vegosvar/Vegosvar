8b98bd26f125fc43578337ce8c9d00b2cffecef8
Tobias Järvelöv <tobias@jarvelov.se>
2016-03-26 20:34:02 +0000

    Rewrite queries with promises

--- src/app/routes/pages/authenticated.js
+++ src/app/routes/pages/authenticated.js
@@ -8,6 +8,7 @@
 var ObjectID = require('mongodb').ObjectID
 var body_parser = require('body-parser')
 var urlencodedParser = body_parser.urlencoded({ extended: false })
+var Promise = require('promise')
 
 module.exports = function (app, resources) {
   var functions = resources.functions
@@ -28,8 +29,8 @@ module.exports = function (app, resources) {
 
   app.get('/installningar/ta-bort/submit', function (req, res) {
     if(req.isAuthenticated()) {
-      var users = resources.collections.users
-      users.remove({ "_id": new ObjectID(req.user._id) }, function(err, doc) {
+      var usersdb = resources.collections.users
+      usersdb.remove({ "_id": new ObjectID(req.user._id) }, function(err, result) {
         if (err) throw err
         res.send('1')
       })
@@ -44,98 +45,119 @@ module.exports = function (app, resources) {
     var website = req.body.website
     var description = req.body.description
 
-    var usersdb = resources.collections.users
-
-    usersdb.update(
-      { _id : new ObjectID(id) },
-      { $set:
-        { "name.display_name": display_name,
-         "info.website":website,
-         "info.description":description }
+    resources.queries.updateUser({
+      _id : new ObjectID(id)
+    }, {
+      $set: {
+        'name.display_name': display_name,
+        'info.website': website,
+        'info.description': description
       }
-    )
-    res.send('1')
+    })
+    .then(function(result) {
+      res.send('1')
+    })
+    .catch(function(err) {
+      console.log(err)
+      res.send('0')
+    })
   })
 
   app.get('/mina-sidor', function (req, res) {
-    var pagesdb = resources.collections.pages
-    var likesdb = resources.collections.likes
-    var votesdb = resources.collections.votes
     var userid = new ObjectID(req.user._id)
 
-    //Get pages user has created
-    pagesdb.find( { "user_info.id": userid }).toArray(function(err, pages) {
-      if (err) throw err
+    var renderObj =  {
+      user: req.user,
+      votes: [],
+      likes: [],
+      loadPageResources: { datatables: true }
+    }
 
-      //Find votes from this user
-      votesdb.find( {"user.id": userid }).toArray(function(err, votes) {
-        if (err) throw err
-
-        var pages_voted_on = []
-        var pages_voted_on_str = []
-        var pages_votes = []
-        for (var i = 0; i < votes.length; i++) {
-          pages_voted_on.push( votes[i].post.id )
-          pages_voted_on_str.push( String(votes[i].post.id ) )
-          pages_votes.push( votes[i].content )
-        }
-
-        //Find pages user voted on
-        pagesdb.find( { _id: { $in: pages_voted_on } } ).toArray(function(err, voted) {
-          if (err) throw err
-
-          var voted_pages = []
-          for (var i = 0; i < voted.length; i++) {
-            var position = pages_voted_on_str.indexOf( String(voted[i]._id) )
-            if( position !== -1 ) {
-              voted_pages.push({
-                page_id: voted[i]._id,
-                url: voted[i].url,
-                title: voted[i].title,
-                content: pages_votes[position]
-              })
-            }
-          }
-
-          //Get likes from this user
-          likesdb.find( { "user.id": userid }).toArray(function(err, likes) {
-            if (err) throw err
-
-            var pages_liked = []
-            for (var i = 0; i < likes.length; i++) {
-              pages_liked.push( likes[i].post.id )
-            }
-
-            //Pages user has liked
-            pagesdb.find( { _id: { $in: pages_liked } }).toArray(function(err, liked) {
-              if (err) throw err
-
-              //Pages user has contributed to (but not created)
-              pagesdb.find({
-                "user_info.id": {
-                  $ne: new ObjectID(userid)
-                },
-                "user_info.contributors": {
-                  $elemMatch: {
-                    id: new ObjectID(userid)
-                  }
-                }
-              }).toArray(function(err, contributions) {
-                if(err) throw err
-
-                res.render('pages', {
-                  user: req.user,
-                  pages: pages,
-                  votes: voted_pages,
-                  likes: liked,
-                  contributions: contributions,
-                  loadPageResources: { datatables: true }
-                })
-              })
-            })
-          })
-        })
+    new Promise.all([
+      //Get the user from the database
+      resources.queries.getUsers({
+        _id: userid
       })
+      .then(function(users) {
+        renderObj.current_user = users[0]
+      }),
+      //Get the pages that this user has created
+      resources.queries.getPages({
+        'user_info.id': userid
+      })
+      .then(function(pages) {
+        renderObj.pages = pages
+      }),
+      //Get all the votes user has cast
+      resources.queries.getVotes({
+        'user.id': userid
+      })
+      .then(function(votes) {
+        //Then loop over all the votes and find the associated page
+        return new Promise.all(votes.map(function(vote) {
+          resources.queries.getPages({
+            _id: new ObjectID(vote.post.id)
+          }, {
+            url: 1,
+            title: 1
+          })
+          .then(function(pages) {
+            if(pages.length > 0) {
+              var page = pages[0]
+
+              renderObj.votes.push({
+                page_id: page._id,
+                url: page.url,
+                title: page.title,
+                content: vote.content
+              })
+            }
+          })
+        }))
+      }),
+      //Get likes this user has given
+      resources.queries.getLikes({
+        'user.id': userid
+      })
+      .then(function(likes) {
+        //Loop over all the likes and get the associated page
+        return new Promise.all(likes.map(function(like) {
+          resources.queries.getPages({
+            _id: new ObjectID(like.post.id)
+          }, {
+            url: 1,
+            title: 1
+          })
+          .then(function(pages) {
+            if(pages.length > 0) {
+              var page = pages[0]
+
+              renderObj.likes.push({
+                page_id: page._id,
+                url: page.url,
+                title: page.title
+              })
+            }
+          })
+        }))
+      }),
+      //Get pages this user has contributed to, but not created
+      resources.queries.getPages({
+        'user_info.id': {
+          $ne: userid
+        },
+        'user_info.contributors': {
+          $elemMatch: {
+            id: userid
+          }
+        }
+      })
+      .then(function(pages) {
+        renderObj.contributions = pages
+      })
+    ])
+    .done(function() {
+      res.render('pages', renderObj)
     })
   })
 
@@ -164,166 +186,139 @@ module.exports = function (app, resources) {
   })
 
   app.get('/ny/:type', function (req, res, next) {
-    var type = req.params.type
-    var mapResources = false
+    var renderObj = {
+      user: req.user,
+      type: req.params.type,
+      loadEditorResources: true,
+      loadDropzoneResources: true,
+      loadValidationResources: true,
+      loadPageResources: {
+        create_page: true
+      }
+    }
 
-    var usersdb = resources.collections.users
-
-    usersdb.find({ _id: new ObjectID(req.user._id), "info.blocked": false }).toArray(function(err, result) {
-      if(result.length > 0) {
-        var types = {
-          'fakta': '1',
-          'recept': '2',
-          'restaurang': '3',
-          'produkt': '4',
-          'butik': '5',
-          'cafe': '6'
-        }
-
-        if(type in types) {
-          var pageType = types[type]
-          var mapResources = (pageType === '3' || pageType === '5' || pageType === '6') ? { autocomplete: true, map: true } : false
-          var pageResources = {
-            create_page: true,
-            youtube: (pageType === '2') ? true : false
-          }
-
-          var categoriesdb = resources.collections.categories
-          categoriesdb.aggregate([
-            {
-              $match: { type: "4" } //TODO, this should be checking with pageType later on
-            }, {
-              $group: {
-                 _id: { id: "$_id", name: "$name", subcategory: "$subcategory" }
-              }
-            }, {
-              $group: {
-                "_id": "$_id.subcategory",
-                "names": {
-                  "$push": {
-                    name: "$_id.name"
-                  }
-                },
-                count: {
-                  $sum: 1
-                }
-              }
-            }, {
-              $sort: {
-                _id: 1
-              }
-            }
-          ], function(err, categories) {
-            if (err) throw err
-
-            res.render('post/'+type, {
-              user: req.user,
-              type: type,
-              loadEditorResources: true,
-              loadDropzoneResources: true,
-              loadValidationResources: true,
-              loadMapResources: mapResources,
-              loadPageResources: pageResources,
-              categories: categories
-            })
-
-          })
-        } else {
-          next()
-        }
+    //Check if current user is blocked
+    resources.queries.getUsers({
+      _id: new ObjectID(req.user._id),
+      'info.blocked': false
+    })
+    .then(function(users) {
+      if(users.length > 0) {
+        return //User checks out, continue
       } else {
-        console.log('blocked')
-        next()
-        //TODO notify user that it is blocked
+        throw new Error('blocked') //User is blocked
+      }
+    })
+    //Check which type of page the user wants to create
+    .then(function() {
+      var types = {
+        'fakta': '1',
+        'recept': '2',
+        'restaurang': '3',
+        'produkt': '4',
+        'butik': '5',
+        'cafe': '6'
+      }
+
+      //Check if type is known
+      if( ! (renderObj.type in types) ) {
+        throw new Error('unknown') //Unknown type
+      }
+
+      var pageType = types[renderObj.type]
+
+      //Load additional browser dependences based on page type
+      renderObj.loadMapResources = (pageType === '3' || pageType === '5' || pageType === '6') ? { autocomplete: true, map: true } : false
+      renderObj.loadPageResources.youtube = (pageType === '2')
+
+      return pageType
+    })
+    //Get the categories for this page type
+    .then(function(type) {
+      return resources.queries.getPageCategories(type)
+      .then(function(categories) {
+        renderObj.categories = categories
+      })
+    })
+    .then(function() {
+      res.render('post/' + renderObj.type, renderObj)
+    })
+    .catch(function(err) {
+      //TODO, handle error better maybe?
+      switch(err.message) {
+        case 'blocked':
+          res.redirect('/blockerad')
+          break
+        case 'unknown':
+          return next()
+          break
+        default:
+          console.log(err)
+          return next()
+          break
       }
     })
   })
 
   app.get('/redigera/:url', function (req, res, next) {
-    var pagesdb = resources.collections.pages
-
-    pagesdb.find({url:req.params.url}).toArray(function(err, doc) {
-      if(doc !== null && doc.length > 0) {
-        var post = doc[0]
-        var type = parseInt(post.type)
-        var page = null
-
-        var mapResources = false
-        var pageResources = {
-          create_page: true
-        }
-
-        switch(type) {
-          case 1:
-            page = 'fakta'
-            break
-          case 2:
-            page = 'recept'
-            pageResources.youtube = true
-            break
-          case 3:
-            page = 'restaurang'
-            mapResources = { autocomplete: true, map: true }
-            break
-          case 4:
-            page = 'produkt'
-            break
-          case 5:
-            page = 'butik'
-            mapResources = { autocomplete: true, map: true }
-            break    
-          case 6:
-            page = 'cafe'
-            mapResources = { autocomplete: true, map: true }
-            break
-          default:
-            return next()
-        }
-
-        var categoriesdb = resources.collections.categories
-        categoriesdb.aggregate([
-          {
-            $match: { type: "4" } //TODO, this should be checking with pageType later on
-          }, {
-            $group: {
-               _id: { id: "$_id", name: "$name", subcategory: "$subcategory" }
-            }
-          }, {
-            $group: {
-              "_id": "$_id.subcategory",
-              "names": {
-                "$push": {
-                  name: "$_id.name"
-                }
-              },
-              count: {
-                $sum: 1
-              }
-            }
-          }, {
-            $sort: {
-              _id: 1
-            }
-          }
-        ], function(err, categories) {
-          if (err) throw err
-
-          if(page !== null) {
-            res.render('post/' + page, {
-              user: req.user,
-              post: post,
-              loadEditorResources: true,
-              loadDropzoneResources: true,
-              loadValidationResources: true,
-              loadMapResources: mapResources,
-              loadPageResources: pageResources,
-              categories: categories
-            })
-          }
-        })
-      } else {
-        next()
+    var renderObj = {
+      user: req.user,
+      loadEditorResources: true,
+      loadDropzoneResources: true,
+      loadValidationResources: true,
+      loadPageResources: {
+        create_page: true
       }
+    }
+
+    resources.queries.getPages({
+      url: req.params.url
+    })
+    .then(function(pages) {
+      if(pages.length <= 0) {
+        throw new Error('404')
+      }
+
+      return pages[0]
+    })
+    .then(function(page) {
+      renderObj.post = page
+
+      //Load additional browser dependences based on page type
+      renderObj.loadMapResources = (page.type === '3' || page.type === '5' || page.type === '6') ? { autocomplete: true, map: true } : false
+      renderObj.loadPageResources.youtube = (page.type === '2')
+
+      return page
+    })
+    //Get the categories for this page type
+    .then(function(page) {
+      return resources.queries.getPageCategories(page.type)
+      .then(function(categories) {
+        renderObj.categories = categories
+        return page
+      })
+    })
+    .then(function(page) {
+      var types = {
+        'fakta': '1',
+        'recept': '2',
+        'restaurang': '3',
+        'produkt': '4',
+        'butik': '5',
+        'cafe': '6'
+      }
+
+      for(var type in types) {
+        if(types[type] === page.type) {
+          renderObj.type = type
+        }
+      }
+
+      res.render('post/' + renderObj.type, renderObj)
+    })
+    .catch(function(err) {
+      console.log(err)
+      return next()
     })
   })
 }
