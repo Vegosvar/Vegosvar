93e121503414908dbcce5801ded3061d80db6d93
Tobias Järvelöv <tobias@jarvelov.se>
2016-04-13 16:05:58 +0000

    Fix variable names

--- src/app/models/revision.js
+++ src/app/models/revision.js
@@ -14,6 +14,12 @@ module.exports = function(resources, models) {
     get: function(query, fields, sort, limit) {
       return resources.queries.find('revisions', query, fields, sort, limit)
     },
+    update: function(query, update, options) {
+      return resources.queries.update('revisions', query, update, options)
+    },
+    insert: function(query) {
+      return resources.queries.insert('revisions', query)
+    },
     accept: function(page_id, id) {
       return models.revision.setStatus(page_id, id, true)
     },
@@ -21,6 +27,8 @@ module.exports = function(resources, models) {
       return models.revision.setStatus(page_id, id, false)
     },
     setStatus: function(page_id, id, status) {
+      page_id = new ObjectID(page_id)
+
       return models.revision.get({
         post_id: page_id
       })
@@ -29,11 +37,12 @@ module.exports = function(resources, models) {
           throw new Error('Could not find a revisions document for page ' + page_id)
         }
 
+        var revision_document = result[0]
         var revisions = result[0].revisions
 
         if(id in revisions) {
           //If this has not been moderated before, decrease the number of revisions pending revision
-          var pending = (revisions[id].meta.accepted === null) ? (doc.pending -1) : doc.pending
+          var pending = (revisions[id].meta.accepted === null) ? (revision_document.pending -1) : revision_document.pending
 
           var data = {
             pending: pending,
@@ -52,18 +61,18 @@ module.exports = function(resources, models) {
             upsert: true
           })
           .then(function(result) {
-            if(result.result.nMatched > 0) {
-              //TODO, document this better
+            if(result.result.n > 0) {
+              //TODO: document this better
 
               if(status === false) {
-                  return result.result.nUpdated
+                  return result.result.nModified
               }
 
               var new_post = data.revisions[id]
               var contributors = (new_post['meta'].user_info instanceof Array) ? new_post['meta'].user_info : [new_post['meta'].user_info]
               delete(new_post['meta'])
 
-              var isodate = utils.newISOdate(new Date(id * 1000))
+              var isodate = resources.utils.newISOdate(new Date(id * 1000))
 
               models.page.update({
                 _id: page_id
@@ -76,13 +85,14 @@ module.exports = function(resources, models) {
                 }
               })
               .then(function(result) {
-                if(result.result.nMatched > 0) {
-                  return result.result.nUpdated
+                if(result.result.n > 0) {
+                  return result.result.nModified
                 } else {
                   throw new Error('No page document matched update query')
                 }
               })
             } else {
+              console.log(result)
               throw new Error('No revision document matched update query')
             }
           })
